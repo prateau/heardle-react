949d6774f15bfa37fb4e89507ef33fca856565b7
prateau <philippe.rateau@outlook.com>
2025-07-15 00:19:47 +0200

    feat: :sparkles: add fuzzy search for search bar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,45 @@
+import React from 'react'
+import Fuse from 'fuse.js'
+import { useHeardleContext } from '../context/HeardleContext'
+import { useTranslation } from 'react-i18next'
+
+const FUSE_BASE_OPTIONS = { // TODO choisir les paramètres
+	// isCaseSensitive: false,
+	// includeScore: false,
+	// ignoreDiacritics: false,
+	// shouldSort: true,
+	// includeMatches: false,
+	// findAllMatches: false,
+	// minMatchCharLength: 1,
+	// location: 0,
+	// threshold: 0.6,
+	// distance: 100,
+	// useExtendedSearch: false,
+	// ignoreLocation: false,
+	// ignoreFieldNorm: false,
+	// fieldNormWeight: 1,
+}
+
 const Search = () => {
+	const { allMusics } = useHeardleContext()
+	const { i18n } = useTranslation()
+
+	const [search, setSearch] = React.useState<string>()
+	
+	const fuse = new Fuse(allMusics, {
+		...FUSE_BASE_OPTIONS,
+		keys: [
+			`name.${i18n.language}`,
+			`category.${i18n.language}`
+		]
+	})
 
 	return (
-		<div>TODO search</div>
+		<div>
+			TODO rendre l'input autocomplete
+			<input onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)}/>
+			{search && fuse.search(search).map(e => <div>{e.item.name[i18n.language]}</div>)}
+		</div>
 	)
 }
 
