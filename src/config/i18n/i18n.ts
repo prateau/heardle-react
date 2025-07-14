b1802197f52cc4ec98a376eb48a2a412a4ce173f
prateau <philippe.rateau@outlook.com>
2025-07-14 16:19:37 +0200

    feat: :sparkles: add language auto detection

--- src/config/i18n/i18n.ts
+++ src/config/i18n/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LngDetector from 'i18next-browser-languagedetector'
 import fr from './fr.json'
 import en from './en.json'
 
@@ -19,9 +20,10 @@ const resources = {
 
 i18n
 	.use(initReactI18next) // passes i18n down to react-i18next
+	.use(LngDetector)
 	.init({
 		resources,
-		lng: Locales.EN, // TODO replace with auto detection
+		fallbackLng: Locales.EN,
 		interpolation: {
 			escapeValue: false // react already safes from xss
 		}
